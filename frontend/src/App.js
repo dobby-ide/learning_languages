c7cb12b2bdb73522dabe132bf5870216a173c208
Fabio Privitera <fabio_privitera@yahoo.it>
2022-04-15 16:23:58 +0300

    App.js: uses axios to retrieve the subjects from server; data is now visible when page is loaded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,28 @@
 import './App.css';
+import axios from 'axios';
 
+import { useState, useEffect } from 'react';
 function App() {
-  return <div className="App">HELLO FROM App.js</div>;
+  const [subject, setSubject] = useState([]);
+  const url = 'http://localhost:3000/admin/subjects';
+  useEffect(() => {
+    retrievingData();
+  }, []);
+  const retrievingData = async () => {
+    const data = await axios.get('http://localhost:3000/admin/subjects');
+
+    setSubject(data.data);
+    console.log(subject);
+  };
+
+  return (
+    <div className="App">
+      {subject.map((singleSubject) => {
+        return <div>{singleSubject.subject_name}</div>;
+      })}
+      <button onClick={retrievingData}></button>
+    </div>
+  );
 }
 
 export default App;
