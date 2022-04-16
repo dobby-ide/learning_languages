d87ba53a2ab3d87929795efaf723e9fb69dbc4ee
Fabio Privitera <fabio_privitera@yahoo.it>
2022-04-16 17:07:23 +0300

    Admin.js can now show the word pairs by pressing on a single subject

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import axios from 'axios';
-
+import Card from './Card';
 import { useState, useEffect } from 'react';
 const Admin = () => {
   const [subject, setSubject] = useState([]);
+  const [wordPairs, setWordPairs] = useState([]);
   const url = 'http://localhost:3000/admin/subjects';
+  const onShowingPairs = async (e) => {
+    e.preventDefault();
+    console.log(e.target.innerHTML);
+    const parameter = e.target.innerHTML;
+    const pairsOfSingleSubject = await axios.get(
+      `http://localhost:3000/admin/subjects/subject?subject=${parameter}`
+    );
+    setWordPairs(pairsOfSingleSubject.data);
+  };
   useEffect(() => {
     retrievingData();
   }, []);
@@ -14,13 +24,35 @@ const Admin = () => {
     setSubject(data.data);
     console.log(subject);
   };
+  console.log(wordPairs);
   return (
-    <div>
-      {subject.map((singleSubject) => {
-        return <div key={singleSubject.id}>{singleSubject.subject_name}</div>;
-      })}
-      <button onClick={retrievingData}></button>
-    </div>
+    <Card className="mainAdmin">
+      <div className="subjectscontainer">
+        {subject.map((singleSubject) => {
+          return (
+            <div
+              key={singleSubject.id}
+              className="subjects"
+              onClick={onShowingPairs}
+            >
+              {singleSubject.subject_name}
+            </div>
+          );
+        })}
+        <button onClick={retrievingData}>add a subject</button>
+      </div>
+      <div className="separator"></div>
+      <div className="mappingpairscontainer">
+        {wordPairs.map((pairs) => {
+          return (
+            <div key={pairs.english} className="admin pairscontainer">
+              <div className="admin english">{pairs.english}</div>
+              <div className="admin finnish">{pairs.finnish}</div>
+            </div>
+          );
+        })}
+      </div>
+    </Card>
   );
 };
 export default Admin;
