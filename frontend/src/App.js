2009dc785600117b0b707e98c6aae4d5a68fea2f
Fabio Privitera <fabio_privitera@yahoo.it>
2022-04-17 17:20:47 +0300

    App.js is now able to switch views (Admin/Child)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
-
+import Child from './Child';
 import './Admin';
-
+import Card from './Card';
 import Admin from './Admin';
+import Filter from './Filter';
+import { useState } from 'react';
 function App() {
+  const [childIsVisible, setChildIsVisible] = useState(false);
+  const [adminIsVisible, setAdminIsVisible] = useState(false);
+  const onChildVisibility = () => {
+    setChildIsVisible(true);
+    setAdminIsVisible(false);
+  };
+  const onAdminVisibility = () => {
+    setChildIsVisible(false);
+    setAdminIsVisible(true);
+  };
   return (
-    <div className="App">
-      <Admin />
-    </div>
+    <Card className="App">
+      <Filter
+        changeChildVisibility={onAdminVisibility}
+        changeAdminVisibility={onChildVisibility}
+      ></Filter>
+      {childIsVisible ? <Admin /> : null}
+      {adminIsVisible ? <Child /> : null}
+    </Card>
   );
 }
 
