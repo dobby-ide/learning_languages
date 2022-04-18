d15e772826eb7a0e4fc10c9d3c99c544ce8d3581
Fabio Privitera <fabio_privitera@yahoo.it>
2022-04-18 11:22:25 +0300

    Child.js: importing Challenge.js and using it

--- frontend/src/Child.js
+++ frontend/src/Child.js
@@ -2,8 +2,55 @@ import Card from './Card';
 import React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import Challenge from './Challenge';
 function Child() {
-  return <div>HELLO from Child view</div>;
+  const url = 'http://localhost:3000/admin/subjects';
+  const [subject, setSubject] = useState([]);
+  const [tableInUse, setTableInUse] = useState('');
+  const [wordPairs, setWordPairs] = useState([]);
+  useEffect(() => {
+    retrievingData();
+  }, []);
+  const storingSubjectName = (e) => {
+    setTableInUse(e);
+  };
+  const onShowingPairs = async (e) => {
+    e.preventDefault();
+    console.log(e.target.innerHTML);
+    storingSubjectName(e.target.innerHTML);
+
+    const parameter = e.target.innerHTML;
+    const pairsOfSingleSubject = await axios.get(
+      `http://localhost:3000/admin/subjects/subject?subject=${parameter}`
+    );
+    setWordPairs(pairsOfSingleSubject.data);
+  };
+  //GET ALL SUBJECTS INTO STATE
+  const retrievingData = async () => {
+    const data = await axios.get(url);
+
+    setSubject(data.data); //subject = [{id:1,subject_name:Animals},{id:2.......}]
+    console.log(subject);
+  };
+  return (
+    <Card className="Child">
+      <div>choose a subject:</div>
+      <div className="subjectscontainer">
+        {subject.map((singleSubject) => {
+          return (
+            <div key={singleSubject.id}>
+              <div className="subjects" onClick={onShowingPairs}>
+                {singleSubject.subject_name}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      {tableInUse !== '' ? <div>{tableInUse}</div> : null}
+      <Challenge subject={tableInUse} pairs={wordPairs} />
+    </Card>
+  );
 }
 
 export default Child;
